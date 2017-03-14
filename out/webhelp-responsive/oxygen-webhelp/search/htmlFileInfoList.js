cc16e31c9150fb576579aac49f84a4d0b9d6a504
saigecmcgovern <saigemcgovern@Saiges-MacBook-Pro.local>
2017-03-14 10:18:34 -0400

    updates and troubleshooting

---
+++ out/webhelp-responsive/oxygen-webhelp/search/htmlFileInfoList.js
@@ -0,0 +1,6 @@
+fil = new Array();
+fil["0"]= "concepts/c-intro.html@@@Going home for Spring Break@@@This year I am going home to work...";
+fil["1"]= "tasks/t-activities.html@@@Things to do@@@This task will explain what we are going to do over spring break when we are not sleeping or eating...";
+fil["2"]= "tasks/t-eating.html@@@Eating arrangements@@@This will tell you how to get food on spring break...";
+fil["3"]= "tasks/t-sleeping.html@@@Sleeping arrangements@@@This task will tell you how to find the place you are sleeping and make the bed...";
+fil["4"]= "tasks/t-wherewearegoingandhowwearetraveling.html@@@Where we are going and how we are getting there@@@This task will tell you where I am going and how I plan on traveling to and from my destination...";
---
+++ out/webhelp-responsive/oxygen-webhelp/search/htmlFileList.js
@@ -0,0 +1,8 @@
+//List of files which are indexed.
+fl = new Array();
+fl["0"]= "concepts/c-intro.html";
+fl["1"]= "tasks/t-activities.html";
+fl["2"]= "tasks/t-eating.html";
+fl["3"]= "tasks/t-sleeping.html";
+fl["4"]= "tasks/t-wherewearegoingandhowwearetraveling.html";
+var doStem = false;searchLoaded = true;
