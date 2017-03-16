408f3ca4c6ab674df2d0714e29586e52dc7b833c
saigecmcgovern <saigemcgovern@Saiges-MacBook-Pro.local>
2017-03-16 10:03:36 -0400

    outputs

--- out/webhelp-responsive/oxygen-webhelp/search/htmlFileInfoList.js
+++
@@ -1,7 +0,0 @@
-fil = new Array();
-fil["0"]= "concepts/c-intro.html@@@Going home for Spring Break@@@This year I am going home to work...";
-fil["1"]= "tasks/t-activities.html@@@Things to do@@@This task will explain what we are going to do over spring break when we are not sleeping or eating...";
-fil["2"]= "tasks/t-eating.html@@@Eating arrangements@@@This will tell you how to get food on spring break...";
-fil["3"]= "tasks/t-sleeping.html@@@Sleeping arrangements@@@This task will tell you how to find the place you are sleeping and make the bed...";
-fil["4"]= "tasks/t-wherewearegoingandhowwearetraveling.html@@@Where we are going and how we are getting there@@@This task will tell you where I am going and how I plan on traveling to and from my destination...";
-fil["5"]= "tasks/tr-traveling.html@@@Help: Traveling@@@You are low on gas. You forgot to get gas before leaving. Now, you are stuck either on Route 7 or 81 and need to find a safe place to stop. Here is how you can make sure you stay safe while getting...";
